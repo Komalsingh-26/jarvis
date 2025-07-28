dfeb0979e85e3b8b2120b3801912de5d2fc3e2dc
Komal <ks26012005@gmail.com>
2025-07-28 11:46:47 +0530

    Face Recognition added

--- engine/command.py
+++ engine/command.py
@@ -3,14 +3,14 @@ import speech_recognition as sr # type: ignore
 import eel
 import time
 def speak(text):
-    #text = str(text)
+    # text = str(text)
     engine = pyttsx3.init('sapi5')
     voices = engine.getProperty('voices') 
     engine.setProperty('voice', voices[0].id)
     engine.setProperty('rate', 174)
     eel.DisplayMessage(text)
     engine.say(text)
-    #eel.receiverText(text)
+    eel.receiverText(text)
     engine.runAndWait()
 
 
@@ -30,8 +30,7 @@ def takecommand():
         eel.DisplayMessage('recognizing....')
         query = r.recognize_google(audio, language='en-in')
         print(f"user said: {query}")
-        eel.DisplayMessage(query)
-        
+        eel.DisplayMessage(query)        
         time.sleep(2)
        
     except Exception as e:
@@ -40,10 +39,15 @@ def takecommand():
     return query.lower()
 
 @eel.expose
-def allCommands(message=1):
-    try:
+def allCommands(message = 1):
+    if message == 1:
         query = takecommand()
         print(query)
+        eel.senderText(query)
+    else:
+        query = message
+        eel.senderText(query)
+    try:
         if "open" in query:
             from engine.features import openCommand
             openCommand(query)
@@ -52,34 +56,20 @@ def allCommands(message=1):
             PlayYoutube(query)
         elif "send message" in query or "phone call" in query or "video call" in query:
             from engine.features import findContact, whatsApp, makeCall, sendMessage
+            message = ""
             contact_no, name = findContact(query)
             if(contact_no != 0):
-                speak("Which mode you want to use whatsapp or mobile")
-                preferance = takecommand()
-                print(preferance)
-
-                if "mobile" in preferance:
-                    if "send message" in query or "send sms" in query: 
-                        speak("what message to send")
-                        message = takecommand()
-                        sendMessage(message, contact_no, name)
-                    elif "phone call" in query:
-                        makeCall(name, contact_no)
-                    else:
-                        speak("please try again")
-                elif "whatsapp" in preferance:
-                    message = ""
-                    if "send message" in query:
-                        message = 'message'
-                        speak("what message to send")
-                        query = takecommand()
+                if "send meassage" in query:
+                    message = 'message'
+                    speak("what message to send")
+                    query = takecommand()
                                         
-                    elif "phone call" in query:
+                elif "phone call" in query:
                         message = 'call'
-                    else:
+                else:
                         message = 'video call'
                                         
-                    whatsApp(contact_no, query, message, name)
+                whatsApp(contact_no, query, message, name)
 
         else:
             print("not run")
